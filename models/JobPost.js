b1098ff0cf7cf3c00eedcfa38887057cce99a796
VoidVampire <yashdsingh2004@gmail.com>
2023-11-10 23:31:26 +0530

    jobpost schema update

--- models/JobPost.js
+++ models/JobPost.js
@@ -3,10 +3,17 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const JobPostSchema = new Schema({
-    title: { type: String, required: true },
+    job_title: { type: String, required: true },
+    created_at: { type: Date, default: Date.now },
+    location: {
+        state: { type: String, required: true },
+        country: { type: String, required: true },
+    },
+    job_type: { type: String, enum: ["REMOTE", "ONSITE", "HYBRID"], default: "REMOTE" },
     description: { type: String, required: true },
-    company: { type: mongoose.Schema.Types.ObjectId, ref: 'Company' },
-    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    salary_range: { type: String, required: true },
+    company: { type: mongoose.Schema.Types.ObjectId, ref: 'Company', required: true },
+    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
 });
 
 module.exports = mongoose.model("JobPost", JobPostSchema);
